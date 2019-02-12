1e4042e9e1622008ac9b8d6719cb90ddcfe7f5b3
Nagy Wesley <nagy.wesley@gmail.com>
2019-02-12 02:47:48 +0200

    use docker-compose && nodemon

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 const express = require('express');
 // Constants
-const PORT = 8080;
+const PORT = 3000;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world Good People!!\n');
+  res.send('GOD is GOOD!!\n');
 });
 
 app.listen(PORT, HOST);
